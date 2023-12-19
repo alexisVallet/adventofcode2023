ed60b34340f9286d014d369b5c5c49b489d9bfa5
Alexis Vallet <alexis.vallet@gmail.com>
2023-12-19 18:53:47 +0900

    Day 17 solution

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,15 +1,16 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use chumsky::prelude::*;
-use petgraph::graph::{DiGraph, NodeIndex};
 use petgraph::algo::astar;
+use petgraph::graph::{DiGraph, NodeIndex};
 
 type Scalar = i32;
 
 type Map = Vec<Vec<Scalar>>;
 
 fn parse_map() -> impl Parser<char, Map, Error = Simple<char>> {
-    let tile = one_of(String::from_iter('0'..='9')).map(|c: char| String::from_iter([c]).parse().unwrap());
+    let tile =
+        one_of(String::from_iter('0'..='9')).map(|c: char| String::from_iter([c]).parse().unwrap());
     let row = tile.repeated().at_least(1);
     row.separated_by(text::newline())
 }
@@ -33,12 +34,13 @@ type Rotation = Scalar;
 struct State {
     pos: Vector,
     dir: Vector,
-    forward_steps: Scalar
+    consecutive_steps: Scalar,
 }
 struct StateGraph {
     graph: DiGraph<State, f64>,
     shape: Vector,
-    init_node: NodeIndex
+    init_node: NodeIndex,
+    min_dir_steps: Scalar,
 }
 
 const NORTH: Vector = [-1, 0];
@@ -50,28 +52,30 @@ fn rotate([x, y]: Vector, rot: Rotation) -> Vector {
     [-rot * y, rot * x]
 }
 
-fn build_state_graph(map: &Map) -> StateGraph {
+fn build_state_graph(map: &Map, min_dir_steps: usize, max_dir_steps: usize) -> StateGraph {
     // Rough upper bounds for number of nodes, edges
     let num_rows = map.len();
     let num_cols = map[0].len();
-    // 0, 1 or 2 steps left, and 4 directions
-    let max_num_nodes = num_cols * num_rows * 3 * 4;
+    // up to max_dir_steps consecutive_steps, and 4 directions
+    let max_num_nodes = num_cols * num_rows * max_dir_steps * 4;
     // No matter the state you can only turn left, right or keep going straight
     let max_num_edges = max_num_nodes * 3;
     let mut state_graph = DiGraph::with_capacity(max_num_nodes, max_num_edges);
     let mut state_to_node: HashMap<State, NodeIndex> = HashMap::new();
     let num_rows = num_rows as Scalar;
     let num_cols = num_cols as Scalar;
+    let min_dir_steps = min_dir_steps as Scalar;
+    let max_dir_steps = max_dir_steps as Scalar;
 
     // Adding all the nodes first
     for i in 0..num_rows {
         for j in 0..num_cols {
             for dir in [NORTH, SOUTH, WEST, EAST] {
-                for forward_steps in 0..3 {
+                for consecutive_steps in 0..max_dir_steps {
                     let state = State {
                         pos: [i, j],
                         dir,
-                        forward_steps,
+                        consecutive_steps,
                     };
                     let node = state_graph.add_node(state);
                     state_to_node.insert(state, node);
@@ -83,17 +87,25 @@ fn build_state_graph(map: &Map) -> StateGraph {
     // Then creating edges from nodes to other nodes they can reach
     for src_node in state_graph.node_indices() {
         let src_state = *(state_graph.node_weight(src_node).unwrap());
-        let directions = vec![rotate(src_state.dir, ROT_LEFT), rotate(src_state.dir, ROT_RIGHT), src_state.dir];
+        let directions = if src_state.consecutive_steps >= min_dir_steps - 1 {
+            vec![
+                rotate(src_state.dir, ROT_LEFT),
+                rotate(src_state.dir, ROT_RIGHT),
+                src_state.dir,
+            ]
+        } else {
+            vec![src_state.dir]
+        };
 
-        for tgt_dir@[tgt_dir_i, tgt_dir_j] in directions {
+        for tgt_dir @ [tgt_dir_i, tgt_dir_j] in directions {
             let mut tgt_state = src_state;
             tgt_state.pos[0] += tgt_dir_i;
             tgt_state.pos[1] += tgt_dir_j;
             tgt_state.dir = tgt_dir;
             if tgt_dir == src_state.dir {
-                tgt_state.forward_steps -= 1;
+                tgt_state.consecutive_steps += 1;
             } else {
-                tgt_state.forward_steps = 2;
+                tgt_state.consecutive_steps = 0;
             }
 
             // If the state is not found, we cannot reach it
@@ -101,7 +113,11 @@ fn build_state_graph(map: &Map) -> StateGraph {
             // moving forward too many times.
             if let Some(tgt_node) = state_to_node.get(&tgt_state) {
                 let [tgt_i, tgt_j] = tgt_state.pos;
-                state_graph.add_edge(src_node, *tgt_node, map[tgt_i as usize][tgt_j as usize] as f64);
+                state_graph.add_edge(
+                    src_node,
+                    *tgt_node,
+                    map[tgt_i as usize][tgt_j as usize] as f64,
+                );
             }
         }
     }
@@ -109,75 +125,74 @@ fn build_state_graph(map: &Map) -> StateGraph {
     // The initial state is special. We choose the initial direction, so
     // we always have forward_steps=2 on the next step, and there is no initial direction.
     let init_state = State {
-        pos: [0,0],
-        dir: NORTH,  // not used
-        forward_steps: 2
+        pos: [0, 0],
+        dir: NORTH, // not used
+        consecutive_steps: 0,
     };
     let init_node = state_graph.add_node(init_state);
     state_to_node.insert(init_state, init_node);
 
-    for tgt_dir@[tgt_dir_i, tgt_dir_j] in [SOUTH, EAST] {
+    for tgt_dir @ [tgt_dir_i, tgt_dir_j] in [SOUTH, EAST] {
         let mut tgt_state = init_state;
         tgt_state.pos[0] += tgt_dir_i;
         tgt_state.pos[1] += tgt_dir_j;
         tgt_state.dir = tgt_dir;
         let tgt_node = state_to_node.get(&tgt_state).unwrap();
         let [tgt_i, tgt_j] = tgt_state.pos;
-        state_graph.add_edge(init_node, *tgt_node, map[tgt_i as usize][tgt_j as usize] as f64);
+        state_graph.add_edge(
+            init_node,
+            *tgt_node,
+            map[tgt_i as usize][tgt_j as usize] as f64,
+        );
     }
-    
+
     StateGraph {
         graph: state_graph,
         init_node,
-        shape: [num_rows, num_cols]
+        shape: [num_rows, num_cols],
+        min_dir_steps: min_dir_steps,
     }
 }
-
-
-fn visualize_path(state_graph: &StateGraph, path: Vec<NodeIndex>) {
-    let path_positions: HashSet<Vector> = HashSet::from_iter(
-        path.into_iter().map(|n| state_graph.graph.node_weight(n).unwrap().pos)
-    );
-    
-    for i in 0..state_graph.shape[0] {
-        for j in 0..state_graph.shape[1] {
-            let s = if path_positions.contains(&[i, j]) {
-                "#"
-            } else {
-                "."
-            };
-            print!("{}", s);
-        }
-        println!("");
-    }
-    println!("");
-}
-
 
 fn find_shortest_path_length(state_graph: &StateGraph) -> Scalar {
     let [num_rows, num_cols] = state_graph.shape;
-    let tgt_pos = [num_rows-1, num_cols-1];
-    let tgt_nodes = state_graph.graph.node_indices().filter(|node|
-        state_graph.graph.node_weight(*node).unwrap().pos == tgt_pos
-    );
-    tgt_nodes.filter_map(|node| {
-        let shortest_path = astar(
-            &(state_graph.graph), 
-            state_graph.init_node,
-            |n| n == node,
-            |e| *(e.weight()), 
-            |src| {
-                let [src_i, src_j] = state_graph.graph.node_weight(src).unwrap().pos;
-                ((num_rows - 1 - src_i).abs() + (num_cols - 1 - src_j).abs()) as f64
-            });
-        shortest_path.map(|(cost, _)|  cost as i32)
-    }).min().unwrap()
+    let tgt_pos = [num_rows - 1, num_cols - 1];
+    let tgt_nodes = state_graph.graph.node_indices().filter(|node| {
+        let state = state_graph.graph.node_weight(*node).unwrap();
+        state.pos == tgt_pos && state.consecutive_steps >= state_graph.min_dir_steps - 1
+    });
+    tgt_nodes
+        .filter_map(|node| {
+            let shortest_path = astar(
+                &(state_graph.graph),
+                state_graph.init_node,
+                |n| n == node,
+                |e| *(e.weight()),
+                |src| {
+                    let [src_i, src_j] = state_graph.graph.node_weight(src).unwrap().pos;
+                    ((num_rows - 1 - src_i).abs() + (num_cols - 1 - src_j).abs()) as f64
+                },
+            );
+            shortest_path.map(|(cost, _)| cost as i32)
+        })
+        .min()
+        .unwrap()
 }
 
 fn main() {
     let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
     let map = parse_map().parse(src).unwrap();
-    let state_graph = build_state_graph(&map);
+    let state_graph_q1 = build_state_graph(&map, 1, 3);
 
-    println!("Question 1 answer is: {}", find_shortest_path_length(&state_graph));
+    println!(
+        "Question 1 answer is: {}",
+        find_shortest_path_length(&state_graph_q1)
+    );
+
+    let state_graph_q2 = build_state_graph(&map, 4, 10);
+
+    println!(
+        "Question 2 answer is: {}",
+        find_shortest_path_length(&state_graph_q2)
+    );
 }
